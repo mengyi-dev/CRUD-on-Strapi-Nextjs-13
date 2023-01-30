d240ac5840861bdd62349e9fefe25249ad34c042
mengyi.yoeng <yoeng.mengyi@z1fsc.com>
2023-01-30 11:21:31 +0700

    First commit -m

---
+++ movie-show/config/server.js
@@ -0,0 +1,8 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: 'https://eb04-163-53-29-18.ngrok.io'
+});
---
+++ movie-show/src/api/movie/routes/movie.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * movie router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::movie.movie');
+
